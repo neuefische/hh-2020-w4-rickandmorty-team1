48730784fda9d1ee5b62ad5fdc729d06b3d48c4e
max bischof <max@bischof.dk>
2020-08-18 21:25:54 +0200

    Style episodes card

--- src/components/pages/Episodes/EpisodeCard.js
+++ src/components/pages/Episodes/EpisodeCard.js
@@ -4,9 +4,20 @@ import styled from 'styled-components'
 function EpisodeCard ({title, date, episode}) {
   return (
   <CardBox>
-    <Title>{title}</Title>
-    <Date>{date}</Date>
-    <Episode>{episode}</Episode>
+    <Title>
+      <TitleDescription>Titel</TitleDescription>
+      <TitleText>{title}</TitleText>
+    </Title>
+    
+    <Date>
+      <DateDescription>Air Date</DateDescription>
+      <DateText>{date}</DateText>
+    </Date>
+    
+    <Episode>
+      <EpisodeDescription>Episode</EpisodeDescription>
+      <EpisodeText>{episode}</EpisodeText>
+    </Episode>
   </CardBox>
   )
 }
@@ -18,19 +29,53 @@ background-color: #121212;
 border-radius: 20px;
 margin: 0px 50px 20px 50px;
 padding: 10px 20px 10px 20px;
+display: grid;
+grid-template-columns: auto auto;
+grid-template-rows: auto auto;
 `
 
-const Title = styled.h2`
+const Title = styled.div`
+    grid-column: 1 / 3;
+    grid-row: 1 / 2;
+`
+
+const TitleText = styled.h2`
+  padding: 0px;
+  margin: 0px;
   color: white;
 `
+const TitleDescription = styled.span`
+  color: #A0A0A0;
+  font-size: 13px;
+`
 
-const Date = styled.span`
+const Date = styled.div`
+  grid-column: 1 / 2;
+  grid-row: 2 / 3;
+`
+
+const DateDescription = styled.span`
+  color: #A0A0A0;
+  font-size: 13px;
+`
+
+const DateText = styled.span`
   color: white;
   display: block;
 `
 
-const Episode = styled.span`
+const Episode = styled.div`
+  grid-column: 2 / 3;
+  grid-row: 2 / 3;
+`
+
+const EpisodeDescription = styled.span`
+  color: #A0A0A0;
+  font-size: 13px;
+`
+const EpisodeText = styled.span`
   color: white;
+  display: block;
 `
 
 
