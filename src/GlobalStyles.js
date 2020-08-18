c640f427d7475bbd3f3feb8220c432e700531658
annaleaschroeder <67912360+annaleaschroeder@users.noreply.github.com>
2020-08-18 18:08:04 +0200

    Insert background image, not yet displayed on page

--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -8,5 +8,10 @@ export default createGlobalStyle`
 body {
     margin: 0;
     font-size: 112.5%;
+    background-image: url('./images/rick_and_morty_bg.jpg');
+    background-position: center; 
+    background-repeat: no-repeat; 
+    background-size: cover;
+    height: 100%;
 }
 `
