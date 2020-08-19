71390a10f344e464f35843dbc8d4fb4db3520d2f
max bischof <max@bischof.dk>
2020-08-19 09:44:40 +0200

    Add game buttons wihout functionality

--- src/components/pages/game/Question.js
+++ src/components/pages/game/Question.js
@@ -4,13 +4,19 @@ import styled from 'styled-components'
 export default function Question ({character}) {
   return <QuestionBox>
     <Image src={character.image}/>
+    <AliveButton>Alive</AliveButton>
+    <DeadButton>Dead</DeadButton>
+    <UnknownButton>Unknown</UnknownButton>
   </QuestionBox>
 }
 
 const QuestionBox = styled.div`
   display:grid;
-  grid-template-columns: 20px 150PX 20px;
+  grid-template-columns: auto 150PX auto;
+  grid-template-rows: auto auto;
   justify-content: center;
+  align-items: center;
+  margin-top: 100px;
 `
 const Image = styled.img`
   border-radius: 50%;
@@ -19,3 +25,37 @@ const Image = styled.img`
   object-fit: cover;
   width: 100%;
 `
+
+const Button = styled.button`
+  text-decoration: none;
+  color: #fff;
+  text-align: center;
+  border: none;
+  border-radius: 2px;
+  height: 36px;
+  line-height: 36px;
+  padding: 0 16px;
+  margin: 20px 0px 0px 0px;
+  text-transform: uppercase;
+  text-align: center;
+  margin: 20px;
+`
+
+const AliveButton = styled(Button)`
+  background-color: #26a69a;
+  grid-row: 1 / 2;
+  grid-column: 1 / 2;
+`
+
+const DeadButton = styled(Button)`
+  background-color: #e63946;
+  grid-row: 1 / 2;
+  grid-column: 3 / 4;
+`
+
+const UnknownButton = styled(Button)`
+  background-color: #A0A0A0;
+  grid-row: 2 / 3;
+  grid-column: 2 / 3;
+`
+
