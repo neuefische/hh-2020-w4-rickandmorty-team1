ee9e8c32c9ef4d352fd58573c8d1f0d1a06bd59a
max bischof <max@bischof.dk>
2020-08-19 10:58:36 +0200

    Move styledComponents for cards to styledComponents.js

---
+++ src/components/styledComponents.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components'
+
+export const Card = styled.div` 
+  background-color: #121212;
+  border-radius: 20px;
+  margin: 0px 50px 20px 50px;
+  padding: 20px 20px 20px 20px;
+  width: 80%;
+`
+
+export const Headline = styled.h2`
+  padding: 0px;
+  margin: 0px;
+  color: white;
+`
+export const Text = styled.span`
+  color: white;
+  display: block;
+`
+
+export const Description = styled.span`
+  color: #A0A0A0;
+  font-size: 13px;
+`
