42a1ab3044fe2e81fc45dd16601aef9bcf6180a5
Martin <office@martinpagels.de>
2020-08-20 11:20:43 +0200

    change wrong import

--- src/components/pages/episodes/EpisodesPage.js
+++ src/components/pages/episodes/EpisodesPage.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import Card from './EpisodeCard'
 import PagesNav from '../../PagesNav'
 import styled from 'styled-components'
-import Navigation from '../../../components/Navigation'
+import Navigation from '../../Navigation'
 
 function EpisodesPage({ setHeadline }) {
   const [episodes, setEpisodes] = useState([])
