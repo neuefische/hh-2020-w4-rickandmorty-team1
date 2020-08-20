baabcb6d20d4b4e14f71daac97e09540695a1f70
Martin <office@martinpagels.de>
2020-08-20 11:25:06 +0200

    Change NavButton style

--- src/components/NavButton/NavButton.js
+++ src/components/NavButton/NavButton.js
@@ -19,16 +19,16 @@ const ButtonWrapper = styled.section`
   background: none;
   border: none;
   box-shadow: 0 0 15px black;
-  border-radius: 15px 15px 0 0;
+  border-radius: 15px;
 `
 const Picture = styled.img`
   border-radius: 15px 15px 0 0;
   width: 220px;
 `
 const ButtonLabel = styled.div`
-  height: 60px;
+  height: 46px;
   opacity: 0.98;
-
+  border-radius: 0 0 15px 15px;
   background: #a67e16;
   display: flex;
   justify-content: center;
