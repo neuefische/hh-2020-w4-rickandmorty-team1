70cb0791e88f4d64d074383825c11405649dc4c9
Martin <office@martinpagels.de>
2020-08-20 13:05:06 +0200

    Add setHeadline to GamePage.js

--- src/components/pages/game/GamePage.js
+++ src/components/pages/game/GamePage.js
@@ -2,9 +2,13 @@ import React, { useState, useEffect } from 'react'
 import Question from './Question'
 import Navigation from '../../../components/Navigation'
 
-export default function GamePage() {
+export default function GamePage({ setHeadline }) {
   const [currentCharacter, setcurrentCharacter] = useState({})
   const [charactersLength, setCharactersLength] = useState()
+
+  useEffect(() => {
+    setHeadline('Dead or Alive')
+  })
 
   useEffect(() => {
     fetch('https://rickandmortyapi.com/api/character')
